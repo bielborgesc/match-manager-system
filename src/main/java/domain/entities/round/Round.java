e7aaff3d99547b928f82d866226eda2c82652caa
Gabriel Borges <gabrielborges565@gmail.com>
2022-05-11 21:03:25 -0300

    criacao das entidades

--- src/main/java/domain/entities/round/Round.java
+++ src/main/java/domain/entities/round/Round.java
@@ -1,4 +1,30 @@
 package domain.entities.round;
 
+import domain.entities.match.Match;
+
+import java.util.LinkedList;
+
 public class Round {
+    private int id;
+    private LinkedList<Match> match;
+
+    public Round(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LinkedList<Match> getMatch() {
+        return match;
+    }
+
+    public void setMatch(Match match) {
+        this.match.add(match);
+    }
 }
