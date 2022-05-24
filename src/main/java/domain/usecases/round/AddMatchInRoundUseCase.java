2810f699608e507add51805d43a61fa32a6ae2a8
Gabriel Borges <gabrielborges565@gmail.com>
2022-05-24 16:37:01 -0300

    ignored

--- src/main/java/domain/usecases/round/AddMatchInRoundUseCase.java
+++ src/main/java/domain/usecases/round/AddMatchInRoundUseCase.java
@@ -1,10 +1,7 @@
 package domain.usecases.round;
 
 import domain.entities.match.Match;
-import domain.entities.player.Player;
-import domain.entities.player.StatusEnum;
 import domain.usecases.match.MatchDAO;
-import domain.usecases.player.PlayerDAO;
 import domain.usecases.utils.exceptions.UnavailablePlayerException;
 
 import java.util.Optional;
@@ -16,11 +13,10 @@ public class AddMatchInRoundUseCase {
         this.matchDAO = matchDAO;
     }
 
-//    public boolean addMatchInRound(Optional<Match> match, Integer idRound) {
-//        if (matchDAO.findOne(player.get().getCpf()).isPresent()) {
-//            Player playerTeam = new Player(player.get().getName(), player.get().getCpf(), player.get().getGender(), idTeam);
-//            return playerDAO.update(playerTeam);
-//        }
-//        throw new UnavailablePlayerException("This player is already on a team");
-//    }
+    public boolean addMatchInRound(Optional<Match> match, Integer idRound) throws UnavailablePlayerException {
+        if (matchDAO.findOne(match.get().getId()).isPresent()) {
+            return matchDAO
+        }
+        throw new UnavailablePlayerException("This player is already on a team");
+    }
 }
