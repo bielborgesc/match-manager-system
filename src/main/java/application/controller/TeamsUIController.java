b9c9ad9f4c9100a5523cd0e169093b60bef95663
Fagner Viana <fagner.viana@domvsit.com.br>
2022-06-19 15:04:10 -0300

    Foi feito as telas de Times

---
+++ src/main/java/application/controller/TeamsUIController.java
@@ -0,0 +1,11 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+
+public class TeamsUIController {
+    public void saveOrUpdate(ActionEvent actionEvent) {
+    }
+
+    public void backToPreviousScene(ActionEvent actionEvent) {
+    }
+}
